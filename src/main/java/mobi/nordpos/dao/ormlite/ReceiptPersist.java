392afabc0975922ecabb0c34085930a7d3a882bd
svininykh <svininykh@gmail.com>
2015-01-13 12:09:08 +0600

    Major: Add Receipt and TicketNumber model and persist.

---
+++ src/main/java/mobi/nordpos/dao/ormlite/ReceiptPersist.java
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2012-2015 Nord Trading Network.
+ *
+ * http://www.nordpos.mobi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package mobi.nordpos.dao.ormlite;
+
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.support.ConnectionSource;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.UUID;
+import mobi.nordpos.dao.model.Receipt;
+
+/**
+ * @author Andrey Svininykh <svininykh@gmail.com>
+ */
+public class ReceiptPersist implements PersistFactory {
+
+    ConnectionSource connectionSource;
+    ReceiptDao receiptDao;
+
+    @Override
+    public void init(ConnectionSource connectionSource) throws SQLException {
+        this.connectionSource = connectionSource;
+        receiptDao = new ReceiptDao(connectionSource);
+    }
+
+    @Override
+    public Receipt read(Object id) throws SQLException {
+        try {
+            return receiptDao.queryForId((UUID) id);
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Receipt> readList() throws SQLException {
+        try {
+            return receiptDao.queryForAll();
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public Receipt find(String column, Object value) throws SQLException {
+        try {
+            QueryBuilder qb = receiptDao.queryBuilder();
+            qb.where().like(column, value);
+            return (Receipt) qb.queryForFirst();
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public Receipt add(Object receipt) throws SQLException {
+        try {
+            Receipt newReceipt = receiptDao.createIfNotExists((Receipt) receipt);
+            receiptDao.assignEmptyForeignCollection(newReceipt, "paymentCollection");
+            receiptDao.assignEmptyForeignCollection(newReceipt, "taxLineCollection");
+            return newReceipt;
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public Boolean change(Object receipt) throws SQLException {
+        try {
+            return receiptDao.update((Receipt) receipt) > 0;
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public Boolean delete(Object id) throws SQLException {
+        try {
+            return receiptDao.deleteById((UUID) id) > 0;
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+}
---
+++ src/main/java/mobi/nordpos/dao/ormlite/TicketNumberPersist.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2012-2015 Nord Trading Network.
+ *
+ * http://www.nordpos.mobi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package mobi.nordpos.dao.ormlite;
+
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.support.ConnectionSource;
+import java.sql.SQLException;
+import java.util.List;
+import mobi.nordpos.dao.model.TicketNumber;
+
+/**
+ * @author Andrey Svininykh <svininykh@gmail.com>
+ */
+public class TicketNumberPersist implements PersistFactory {
+
+    ConnectionSource connectionSource;
+    TicketNumberDao ticketNumberDao;
+
+    @Override
+    public void init(ConnectionSource connectionSource) throws SQLException {
+        this.connectionSource = connectionSource;
+        ticketNumberDao = new TicketNumberDao(connectionSource);
+    }
+
+    @Override
+    public TicketNumber read(Object id) throws SQLException {
+        try {
+            return ticketNumberDao.queryForId((Integer) id);
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public List<TicketNumber> readList() throws SQLException {
+        try {
+            return ticketNumberDao.queryForAll();
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public TicketNumber find(String column, Object value) throws SQLException {
+        try {
+            QueryBuilder qb = ticketNumberDao.queryBuilder();
+            qb.where().like(column, value);
+            return (TicketNumber) qb.queryForFirst();
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public TicketNumber add(Object ticketNumber) throws SQLException {
+        try {
+            return ticketNumberDao.createIfNotExists((TicketNumber) ticketNumber);
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public Boolean change(Object ticketNumber) throws SQLException {
+        TicketNumber number = (TicketNumber) ticketNumber;
+        try {
+            return ticketNumberDao.updateId(number, number.getId() + 1) > 0;
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+    @Override
+    public Boolean delete(Object id) throws SQLException {
+        try {
+            return ticketNumberDao.deleteById((Integer) id) > 0;
+        } finally {
+            if (connectionSource != null) {
+                connectionSource.close();
+            }
+        }
+    }
+
+}
